920ae4834f5837d41748f7c570a9e46946d11fde
Abdul Qavi <asadm8975@gmail.com>
2025-03-26 12:38:23 +0500

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const API_URL = `${env.VITE_APP_BASE_NAME}`;
   const PORT = `${'3000'}`;
-
+  base: '/', 
   return {
     server: {
       // this ensures that the browser opens upon server start
